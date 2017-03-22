d1bf1153815d2d5d12dcfdd0041df4cfe633acb9
Ningsih Nababan <ningsih.14115035@student.itera.ac.id>
2017-03-22 12:37:58 +0700

    Add files via upload

---
+++ 14116136_DasproC_02.cpp
@@ -0,0 +1,346 @@
+#include <iostream>
+using namespace std;
+
+void ubah (int a){
+	int b,c,d;
+	b = a / 10;
+	c = a % 10;
+	d = (a/10)%10;
+	if (a==1){
+		cout << "satu";
+	}
+	if (a==2){
+		cout <<"dua";
+	}
+	if (a==3){
+		cout <<"tiga";
+	}
+	if (a==4){
+		cout <<"empat";
+	}
+	if (a==5){
+		cout <<"lima";
+	}
+	if (a==6){
+		cout <<"enam";
+	}
+	if (a==7){
+		cout <<"tujuh";
+	}
+	if (a==8){
+		cout <<"delapan";
+	}
+	if (a==9){
+		cout <<"sembilan";
+	}
+	if (a==10){
+		cout <<"sepuluh";
+	}
+	if (a == 11){
+		cout<<"sebelas";
+	}
+	if (a == 12){
+		cout<<"duabelas";
+	}
+	if (a == 13){
+		cout<<"tigabelas";
+	}
+	if (a == 14){
+		cout<<"empatbelas";
+	}
+	if (a == 15){
+		cout<<"limabelas";
+	}
+	if (a == 16){
+		cout<<"enambelas";
+	}
+	if (a == 17){
+		cout<<"tujuhbelas";
+	}
+	if (a == 18){
+		cout<<"delapanbelas";
+	}
+	if (a == 19){
+		cout<<"sembilanbelas";
+	}
+	if (a == 20){
+		cout<<"dua puluh";
+	}
+	if (a == 30){
+		cout<<"tiga puluh";
+	}
+	if (a == 40){
+		cout<<"empat puluh";
+	}
+	if (a == 50){
+		cout<<"lima puluh";
+	}
+	if (a == 60){
+		cout<<"enam puluh";
+	}
+	if (a == 70){
+		cout<<"tujuh puluh";
+	}
+	if (a == 80){
+		cout<<"delapan puluh";
+	}
+	if (a == 90){
+		cout<<"sembilan puluh";
+	}
+	if (d==20) {
+		cout << "dua ratus";
+		if (b==2){
+		cout<<"dua puluh";
+		if (c==1){
+			cout<<" satu";
+		}
+		else if (c==2){
+			cout<<"dua";
+		}
+		else if (c==3){
+			cout<<" tiga";
+		}
+		else if (c==4){
+			cout<<" empat";
+		}
+		else if (c==5){
+			cout<<" lima";
+		}
+		else if (c==6){
+			cout<<" enam";
+		}
+		else if (c==7){
+			cout<<" tujuh";
+		}
+		else if (c==8){
+			cout<<" delapan";
+		}
+		else if (c==9){
+			cout<<" sembilan";
+		}
+	}
+	}
+	
+	if (b==3){
+		cout<<"tiga puluh";
+		if (c==1){
+			cout<<" satu";
+		}
+		else if (c==2){
+			cout<<" dua";
+		}
+		else if (c==3){
+			cout<<" tiga";
+		}
+		else if (c==4){
+			cout<<" empat";
+		}
+		else if (c==5){
+			cout<<" lima";
+		}
+		else if (c==6){
+			cout<<" enam";
+		}
+		else if (c==7){
+			cout<<" tujuh";
+		}
+		else if (c==8){
+			cout<<" delapan";
+		}
+		else if (c==9){
+			cout<<" sembilan";
+		}
+	
+}
+	if (b==4){
+		cout<<"empat puluh";
+		if (c==1){
+			cout<<" satu";
+		}
+		else if (c==2){
+			cout<<" dua";
+		}
+		else if (c==3){
+			cout<<" tiga";
+		}
+		else if (c==4){
+			cout<<" empat";
+		}
+		else if (c==5){
+			cout<<" lima";
+		}
+		else if (c==6){
+			cout<<" enam";
+		}
+		else if (c==7){
+			cout<<" tujuh";
+		}
+		else if (c==8){
+			cout<<" delapan";
+		}
+		else if (c==9){
+			cout<<" sembilan";
+		}
+	}
+	if (b==5){
+		cout<<"lima puluh";
+		if (c==1){
+			cout<<" satu";
+		}
+		else if (c==2){
+			cout<<" dua";
+		}
+		else if (c==3){
+			cout<<" tiga";
+		}
+		else if (c==4){
+			cout<<" empat";
+		}
+		else if (c==5){
+			cout<<" lima";
+		}
+		else if (c==6){
+			cout<<" enam";
+		}
+		else if (c==7){
+			cout<<" tujuh";
+		}
+		else if (c==8){
+			cout<<" delapan";
+		}
+		else if (c==9){
+			cout<<" sembilan";
+		}
+	}
+	if (b==6){
+		cout<<"enam puluh";
+		if (c==1){
+			cout<<" satu";
+		}
+		else if (c==2){
+			cout<<" dua";
+		}
+		else if (c==3){
+			cout<<" tiga";
+		}
+		else if (c==4){
+			cout<<" empat";
+		}
+		else if (c==5){
+			cout<<" lima";
+		}
+		else if (c==6){
+			cout<<" enam";
+		}
+		else if (c==7){
+			cout<<" tujuh";
+		}
+		else if (c==8){
+			cout<<" delapan";
+		}
+		else if (c==9){
+			cout<<" sembilan";
+		}
+	}
+	if (b==7){
+		cout<<"tujuh puluh";
+		if (c==1){
+			cout<<" satu";
+		}
+		else if (c==2){
+			cout<<" dua";
+		}
+		else if (c==3){
+			cout<<" tiga";
+		}
+		else if (c==4){
+			cout<<" empat";
+		}
+		else if (c==5){
+			cout<<" lima";
+		}
+		else if (c==6){
+			cout<<" enam";
+		}
+		else if (c==7){
+			cout<<" tujuh";
+		}
+		else if (c==8){
+			cout<<" delapan";
+		}
+		else if (c==9){
+			cout<<" sembilan";
+		}
+	}
+	if (b==8){
+		cout<<"delapan puluh";
+		if (c==1){
+			cout<<" satu";
+		}
+		else if (c==2){
+			cout<<" dua";
+		}
+		else if (c==3){
+			cout<<" tiga";
+		}
+		else if (c==4){
+			cout<<" empat";
+		}
+		else if (c==5){
+			cout<<" lima";
+		}
+		else if (c==6){
+			cout<<" enam";
+		}
+		else if (c==7){
+			cout<<" tujuh";
+		}
+		else if (c==8){
+			cout<<" delapan";
+		}
+		else if (c==9){
+			cout<<" sembilan";
+		}
+	}
+	if (b==9){
+		cout << "sembilan puluh";
+		if (c==1){
+			cout<<" satu";
+		}
+		else if (c==2){
+			cout<<" dua";
+		}
+		else if (c==3){
+			cout<<" tiga";
+		}
+		else if (c==4){
+			cout<<" empat";
+		}
+		else if (c==5){
+			cout<<" lima";
+		}
+		else if (c==6){
+			cout<<" enam";
+		}
+		else if (c==7){
+			cout<<" tujuh";
+		}
+		else if (c==8){
+			cout<<" delapan";
+		}
+		else if (c==9){
+			cout<<" sembilan";
+		}
+	}
+}
+int main () {
+	int input;
+	while (input!=-999){
+	cin>>input;
+	ubah(input);
+	cout << endl;
+	}
+	cout<<"S E L E S A I"<<endl;
+	
+	return 0;
+}
